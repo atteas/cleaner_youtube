f2e4f077d616034f99ae7b5acf6aa606856db553
atteas <atte.aaltosetala@gmail.com>
2025-07-16 17:16:07 +0300

    Initial commit. Shorts-blocker almost ready if no other places to see shorts are not found.

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center"> C l e a n e r&nbsp;&nbsp;Y o u T u b e</h1>
+<div align="center">
+    <img src="readmeImages/cleanerYoutubeLogo.png" alt="cleaner youtube logo">
+</div>
+<p align="center">The purpose of this project is to be able to make the YouTube a cleaner place for everyone by being able to block channels even from search, block shorts-videos completely and change the colours a bit.</p>
+
+<br>
---
+++ scripts/shorts_blocker.js
@@ -0,0 +1,64 @@
+/***************************
+
+  CYT - Cleaner YouTube
+
+  programmer: atteas (github)
+  version: 0.01
+
+***************************/
+
+
+/*************** MAIN *****************/
+let lastUrl = location.href;
+
+//Observer to document.body
+const observer = new MutationObserver(mutations => {
+    for (const mutation of mutations){
+        for (const node of mutation.addedNodes){
+            //Make sure node is an element
+            if (!(node instanceof HTMLElement)) continue;
+
+            const nodeTagName = node.tagName.toLowerCase();
+
+            //Remove shorts shelfs
+            if (nodeTagName == "ytd-reel-shelf-renderer" || nodeTagName == "ytd-rich-shelf-renderer" || nodeTagName == "grid-shelf-view-model"){
+                console.log("Shorts shelf found: ", node);
+                node.remove();
+            }
+
+            //Remove shorts videos
+            else if (nodeTagName == "a"){
+                if (node.href.includes("shorts")){
+                    const shortsElement = node.closest("ytd-video-renderer");
+                    if (shortsElement){
+                        console.log("Shorts video found: ", shortsElement);
+                        shortsElement.remove();
+                    }
+                }
+            }
+
+            //Remove shorts from channel
+            else if (nodeTagName == "ytm-shorts-lockup-view-model-v2"){
+                const shortsHolder = node.closest("div#contents");
+                if (shortsHolder){
+                    console.log("Shorts holder found in channel page: ", shortsHolder);
+                    shortsHolder.remove();
+                }
+            }
+
+            //Remove shorts tab from the side
+            else if (nodeTagName == "ytd-mini-guide-entry-renderer"){
+                if (node.getAttribute?.("aria-label") == "Shorts"){
+                    console.log("Shorts tab found from side: ", node);
+                    node.remove();
+                }
+            }
+        }
+    }
+});        
+
+//Observer config, target and initialization
+observer.observe(document.body, { // first: targetNode, second: config
+    childList: true,
+    subtree: true
+});
